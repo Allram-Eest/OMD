fd4505b16642f916a3cd634cfa05ca91135bc6ea
Allram-Eest <da7413sj-s@student.lu.se>
2023-10-18 15:29:12 +0200

    Extentor

---
+++ src/ExTentor/ExTenta2021/ExTenta2017/up3.java
@@ -0,0 +1,29 @@
+package ExTentor.ExTenta2021.ExTenta2017;
+
+import java.util.Optional;
+
+public class up3 {
+    
+}
+
+interface Company {
+    public Optional<Department> getDepartment(String code);
+}
+
+interface Department {
+    public Optional<Employee> getEmployee(int id);
+
+}
+
+interface Employee {
+    public String getName();
+
+}
+
+class FindName {
+
+    public String findName(Company company, String deptCode, int empCode) {
+        return company.getDepartment(deptCode) //Optional<Department>
+                        .flatMap(d -> d.getEmployee(empCode)) //Optional<Employee>
+                        .map(o -> o.getName().orElse("Not Found"));
+}
